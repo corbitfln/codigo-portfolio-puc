5ee059ef227ca36041fd4301ff9de4130c3c4198
rogerio.ribeiro <rogerio.ribeiro@dataprev.gov.br>
2023-06-17 23:48:40 -0300

    Terminando as aulas com o Devsoutinho

---
+++ 03-minibrowser/map.js
@@ -0,0 +1,16 @@
+//TDD Test Driven Development
+var novoMapa = new Map();
+
+class HistoryMap {
+}
+
+
+// 11/11/2011 as 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL', {title: 'Titulo do site - URL', url: 'url sozinha'});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+novoMapa.set('11/11/2011 - 23:31 - URL', {});
+
+
+
+var resultado = novoMapa.get('11/11/2011 - 23:30 - URL');       
+console.log(resultado); 
